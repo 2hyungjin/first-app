a532bf50be26bb4313d2b2844bd0072e4db28703
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-06-20 12:16:34 +0900

    modal

---
+++ FirstApplication/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  FirstApplication
+//
+//  Created by 이형진 on 2022/06/20.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func dismissModal(){
+        dismiss(animated: true)
+    }
+   
+
+}
