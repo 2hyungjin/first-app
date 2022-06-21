092ec1d4eda39ffbd2985a54bdb0226c4be96c1c
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-06-21 15:44:53 +0900

    tableView

--- FirstApplication/FirstApplication/SecondViewController.swift
+++ FirstApplication/FirstApplication/SecondViewController.swift
@@ -7,16 +7,66 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak var tabelView: UITableView!
+    let cellIdentifier:String = "cell"
+    let customCellIdentifier:String = "customCell"
+    
+    let korean:[String] = ["가","나","다","라","마","바","사","아","자","차","카","타","파","하"]
+    let english:[String] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var array:[String] = []
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section{
+        case 0:
+            return korean.count
+        case 1:
+            return english.count
+        default:
+            return array.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section < 2{
+            let cell : UITableViewCell = tabelView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+            let text : String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+            cell.textLabel?.text = text
+            return cell
+        }else{
+            let cell : CustomTableViewCell = tableView.dequeueReusableCell(withIdentifier: customCellIdentifier, for: indexPath) as! CustomTableViewCell
+            cell.leftLabel?.text = String(indexPath.row)
+            cell.rightLabel?.text = array[indexPath.row]
+            return cell
+            
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section==2{
+            return nil
+        }
+        return section == 0 ? "한글" : "영어"
+    }
+    
+    
+    @IBAction func touchUpAddButton(_ sender: Any) {
+        array.append("a")
+        tabelView.reloadSections(IndexSet(2...2), with: UITableView.RowAnimation.automatic)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     @IBAction func popNaviagion(){
         self.navigationController?.popViewController(animated: true)
     }
     
-
-
+    
+    
 }
--- FirstApplication/ThirdViewController.swift
+++ FirstApplication/ThirdViewController.swift
@@ -8,15 +8,32 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    let dateFormatter : DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+    
+    
+    
+    @IBAction func didDatePickerValueChanged(_ sender : UIDatePicker){
+        let date: Date = sender.date
+        let dateString = dateFormatter.string(from: date)
+        
+        dateLabel.text = dateString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func dismissModal(){
         dismiss(animated: true)
     }
-   
-
+    
+    
 }
