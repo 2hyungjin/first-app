20cbdd440552cc77fff82efac328c8a781010e35
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-06-23 15:49:00 +0900

    collection view

---
+++ FirstApplication/FirstApplication/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  FirstApplication
+//
+//  Created by 이형진 on 2022/06/23.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet var indexLabel : UILabel!
+    @IBOutlet var contentLabel : UILabel!
+    
+}
--- FirstApplication/FirstApplication/CollectionViewController.swift
+++ FirstApplication/FirstApplication/CollectionViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class CollectionViewController: UIViewController,UICollectionViewDataSource {
     var array:[String]=[]
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return array.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        guard let cell:CollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionViewCell else{
+            return UICollectionViewCell()
+        }
+        cell.indexLabel.text = String(indexPath.item)
+        cell.contentLabel.text = array[indexPath.item]
         return cell
     }
     
@@ -26,5 +31,9 @@ class CollectionViewController: UIViewController,UICollectionViewDataSource {
         print(array)
     }
     
-
+    @IBAction func touchUpAddButton(_ sender: UIButton){
+        array.append("A")
+        collectionView.reloadData()
+        
+    }
 }
